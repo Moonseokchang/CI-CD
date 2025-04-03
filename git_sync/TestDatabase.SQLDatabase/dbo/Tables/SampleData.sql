dd3a5ebeb555d1b5000befb2458a9c040538f327
Moonseokchang <moon91@qcells.com>
2025-04-03 10:47:52 +0900

    Committing 1 item from workspace d132e3ad-f686-4fcc-bbc3-b94fca3ba1d5

---
+++ git_sync/TestDatabase.SQLDatabase/dbo/Tables/SampleData.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[SampleData] (
+    [id]   INT            IDENTITY (1, 1) NOT NULL,
+    [name] NVARCHAR (100) NOT NULL,
+    [age]  INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+GO
+
