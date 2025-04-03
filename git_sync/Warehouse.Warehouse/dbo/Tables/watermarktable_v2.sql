4b8ea95e9d4a2efd72bcbe4f3201034add07018c
Moonseokchang <moon91@qcells.com>
2025-04-03 11:12:42 +0900

    [Add] Add Test Warehouse

---
+++ git_sync/Warehouse.Warehouse/dbo/Tables/watermarktable_v2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[watermarktable_v2] (
+
+	[source_database] varchar(255) NULL, 
+	[source_table] varchar(255) NULL, 
+	[target_database] varchar(255) NULL, 
+	[target_table] varchar(255) NULL, 
+	[watermark_value] varchar(255) NULL
+);
+
