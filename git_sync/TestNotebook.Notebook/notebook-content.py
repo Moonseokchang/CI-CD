c2efb86feaed32a4b749bb18e26bbee8677c4966
Moonseokchang <moon91@qcells.com>
2025-04-03 09:52:41 +0900

    Committing 2 items from workspace d132e3ad-f686-4fcc-bbc3-b94fca3ba1d5

---
+++ git_sync/TestNotebook.Notebook/notebook-content.py
@@ -0,0 +1,92 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "b050fba3-ff19-4473-9ff3-8b3d31a43776",
+# META       "default_lakehouse_name": "Lakehouse",
+# META       "default_lakehouse_workspace_id": "d132e3ad-f686-4fcc-bbc3-b94fca3ba1d5",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "b050fba3-ff19-4473-9ff3-8b3d31a43776"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+import pandas as pd
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.read.format("csv").option("header","true").load("Files/wemsdb_wems_tb_opr_alarm_v2.csv")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# df
+
+# CELL ********************
+
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+delta_table_name='TempAlarmTable'
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df.write.mode("overwrite").format("delta").saveAsTable(delta_table_name)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
